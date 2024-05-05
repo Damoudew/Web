0483ee9bb880b229135feacebc750380ea01cc16
Gleb <gleb.martynenko.a@mail.ru>
2024-05-05 17:28:33 +0300

    5

--- lab5/4/app.jsx
+++ lab5/4/app.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom"; 
 
 const navlinkItems = [
-<ul className="nav">
     <li className="nav-item">
       <a className="nav-link" href="#">Link to google.com</a>
-    </li>
+    </li>,
     <li className="nav-item">
       <a className="nav-link" href="#">Link to facebook.com</a>
-    </li>
+    </li>,
     <li className="nav-item">
       <a className="nav-link" href="#">Link to amazon.com</a>
     </li>
-</ul>
 ];
 
 const content = <ul className="nav">{navlinkItems}</ul>;
